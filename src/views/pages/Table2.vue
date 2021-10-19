1b7ecca31979b9e501426331658eec3545259723
유민호 <wingedcompany@gmail.com>
2021-10-19 23:52:55 +0900

    start 9 - Firebase Storage 활용 실습

--- src/views/pages/Table2.vue
+++ src/views/pages/Table2.vue
@@ -2,6 +2,9 @@
   <div>
     <vx-card title="테이블 테스트">
       <a-table :columns="columns" :data-source="data">
+        <template slot="img" slot-scope="record">
+          <img v-if="record.img" :src="record.img" alt="record.alt" style="width:50px; height:50px;">
+        </template>
         <template slot="operation" slot-scope="record">
           <vs-button @click="() => edit(record)">선택</vs-button>
         </template>
@@ -9,12 +12,26 @@
       </a-table>
     </vx-card>
     <vx-card title="데이터 입력" style="margin-top:50px">
-      <vs-input class="inputx" placeholder="Name" v-model="name"></vs-input>
-      <vs-input class="inputx" placeholder="Age" v-model="age"></vs-input>
-      <vs-input class="inputx" placeholder="Address" v-model="address"></vs-input>
-      <vs-button @click="onAddData()" size="small">신규데이터추가</vs-button>
-      <vs-button @click="onUpdateData()" size="small">업데이트</vs-button>
-      <vs-button @click="onDeleteData()" size="small">삭제</vs-button>
+      <div style="display:flex; margin-bottom: 10px">
+        <vs-input class="inputx" placeholder="Name" v-model="name" style="margin-left:10px"></vs-input>
+        <vs-input class="inputx" placeholder="Age" v-model="age" style="margin-left:10px"></vs-input>
+        <vs-input class="inputx" placeholder="Address" v-model="address" style="margin-left:10px"></vs-input>
+      </div>
+      <vs-button @click="onAddData()" size="small" style="margin-left:10px">신규데이터추가</vs-button>
+      <vs-button @click="onUpdateData()" size="small" style="margin-left:10px">업데이트</vs-button>
+      <vs-button @click="onDeleteData()" size="small" style="margin-left:10px">삭제</vs-button>
+
+      <div>이미지 업로드</div>
+
+      <div class="col-md-10" style="margin-bottom: 10px" v-if="key">
+        <a-upload
+            :fileList="fileList"
+            :beforeUpload="beforeUpload"
+            @change="handleChange">
+          <a-button><a-icon type="upload"></a-icon> 업로드 </a-button>
+        </a-upload>
+        <img v-if="img" :src="img" alt="alt" style="width: 50px; height: 50px;"/>
+      </div>
     </vx-card>
   </div>
 </template>
@@ -25,7 +42,10 @@ import firebase from "firebase";
 export default {
   data() {
     return {
+      fileList: "",
       name: "",
+      img: "",
+      alt: "",
       age: "",
       address: "",
       key: "",
@@ -49,6 +69,11 @@ export default {
           ellipsis: true,
         },
         {
+          title: 'img',
+          key: 'img',
+          scopedSlots: {customRender: "img"}
+        },
+        {
           title: 'action',
           key: 'address 1',
           scopedSlots: {customRender: "operation"}
@@ -61,18 +86,74 @@ export default {
     this.onLoadData();
   },
   methods: {
+    beforeUpload(file) {
+      this.fileList = [...this.fileList, file];
+      return false
+    },
+    handleChange(info) {
+      if (info.file.status === "removed") {
+        this.handleRemove();
+      } else {
+        this.createFile(info.file, 0);
+      }
+    },
+    handleRemove() {
+      this.fileList = [];
+      this.img = "";
+    },
+    createFile(file, idx) {
+      if (!file.type.match("image.*")) {
+        alert("not image!");
+        this.handleRemove();
+        return;
+      }
+      var reader = new FileReader();
+      var vm = this;
+      reader.onload = function (e) {
+        vm.saveToFirebaseStorage(e, file, idx);
+      };
+      reader.readAsDataURL(file)
+    },
+    saveToFirebaseStorage(evt, items, idx) {
+      var _key = new Date().getTime() + idx;
+      var self = this;
+      var storageRef = firebase.storage().ref();
+      var _name = items.name.replace(/[~`!#$%^&*+=\-\[\]\\';,/{}()|":<>?]/g, "");
+      var user = firebase.auth().currentUser;
+      var uploadTask = storageRef.child("data/" + user.uid + "/" + _key + "/" + _name).put(items);
+      uploadTask.on("state_changed", function (snapshot) {
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                console.log("Upload is paused");
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running' console.log("Upload is running");
+                break;
+            }
+          },
+          function (error) {
+            console.log(error);
+          },
+          function () {
+            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+              self.img = downloadURL;
+            })
+          });
+    },
     edit(r) {
-      console.log(r)
       this.name = r.name;
       this.age = r.age;
       this.address = r.address;
       this.key = r.key;
+      this.img = r.img;
     },
     onInitData() {
       this.key = "";
       this.name = "";
       this.age = "";
       this.address = "";
+      this.img = "";
     },
     onRefreshData() {
       var self = this;
@@ -81,6 +162,7 @@ export default {
           item.name = self.name;
           item.age = self.age;
           item.address = self.address;
+          item.img = self.img;
         }
         return item;
       })
@@ -110,7 +192,8 @@ export default {
       var updateJson = {
         name: self.name,
         age: self.age,
-        address: self.address
+        address: self.address,
+        img: self.img
       }
       _ref.update(updateJson).then(() => {
         self.onRefreshData();
@@ -130,7 +213,11 @@ export default {
       })
     },
     onAddData() {
-      console.log("aaaaa")
+      if(this.key) {
+        this.onInitData();
+        alert('불가');
+        return;
+      }
       var db = firebase.firestore();
       var self = this;
       var user = firebase.auth().currentUser;
@@ -140,6 +227,7 @@ export default {
           name: self.name,
           age: self.age,
           address: self.address,
+          img: self.img
         }
         db.collection("bbs").add(addData).then(function (mRef) {
           addData.key = mRef.id;
