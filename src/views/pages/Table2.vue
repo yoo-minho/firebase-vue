344456129ba86f964920e969d379a1ffdefa1023
유민호 <wingedcompany@gmail.com>
2021-10-19 22:46:16 +0900

    start 6 - Firebase를 활용한 CRUD - 2

--- src/views/pages/Table2.vue
+++ src/views/pages/Table2.vue
@@ -1,20 +1,33 @@
 <template>
-  <a-table :columns="columns" :data-source="data">
-    <a slot="name" slot-scope="text">{{ text }}</a>
-  </a-table>
+  <div>
+    <vx-card title="테이블 테스트">
+      <a-table :columns="columns" :data-source="data">
+        <a slot="name" slot-scope="text">{{ text }}</a>
+      </a-table>
+    </vx-card>
+    <vx-card title="데이터 입력" style="margin-top:50px">
+      <vs-input class="inputx" placeholder="Name" v-model="name"></vs-input>
+      <vs-input class="inputx" placeholder="Age" v-model="age"></vs-input>
+      <vs-input class="inputx" placeholder="Address" v-model="address"></vs-input>
+      <vs-button
+          @click="onAddData()"
+          icon-pack="feather"
+          icon="icon-chevrons-right"
+          icon-after
+          class="shadow-md w-full lg:mt-0 mt-4">신규데이터추가</vs-button>
+    </vx-card>
+  </div>
 </template>
 
 <script>
-// {
-//   'id': 1,
-//     'name': 'Leanne Graham',
-//     'username': 'Bret',
-//     'email': 'Sincere@april.biz',
-//     'website': 'hildegard.org'
-// }
+import firebase from "firebase";
+
 export default {
   data() {
     return {
+      name: "",
+      age: "",
+      address: "",
       columns: [
         {
           title: 'Name',
@@ -57,6 +70,25 @@ export default {
           tags: ['cool', 'teacher'],
         },
       ]
+    }
+  },
+  methods: {
+    onAddData() {
+      console.log("aaaaa")
+      var db = firebase.firestore();
+      var self = this;
+      var user = firebase.auth().currentUser;
+      if (user) {
+        db.collection("bbs")
+            .add({
+              uid: user.uid,
+              name: self.name,
+              age: self.age,
+              address: self.address,
+            }).then(function () {
+          alert("적용!")
+        })
+      }
     }
   }
 }
